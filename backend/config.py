ec5a68a6c3cd80121c96233139df1d2ada253fc9
ABHYUDAY SHARMA <abhyudaysharma665@gmail.com>
2025-12-07 19:55:40 +0530

    updated

--- backend/config.py
+++ backend/config.py
@@ -1,16 +1,25 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
-    database_hostname: str = "localhost" # Default fallback
+    database_hostname: str = "localhost"
     database_port: str = "5432"
     database_password: str
     database_name: str
     database_username: str
+
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
 
-    class Config:
-        env_file = ".env"
+    gemini_api_key: str | None = None
+    tavily_api_key: str | None = None
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="forbid"   # keeps strict validation
+    )
+
 
 settings = Settings()
